cda49f91ffa3ac9405237ff75c32270a49ac19da
uTulipan <102763921+uTulipan@users.noreply.github.com>
2024-07-23 16:13:43 -0400

    Versión principal del juego

---
+++ app.js
@@ -0,0 +1,61 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = (texto);
+    return;
+}
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+    if (numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento("h1", `Acertaste el número en ${intentos} ${(intentos == 1) ? "intento" : "intentos"}`);
+        asignarTextoElemento("p", "");
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else {
+        if (numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento("p", "El número es menor que el que elegiste");
+        } else{
+            asignarTextoElemento("p", "El número es mayor que el que elegiste");
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return; 
+}
+
+function limpiarCaja(){
+    document.querySelector("#valorUsuario").value = "";
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento ("p", "Ya se sortearon todos los números posibles");
+        document.querySelector ("#intent").setAttribute ("disabled", "true");
+    } else {
+        if (listaNumerosSorteados.includes(numeroGenerado)){ // el includes recorre la lista
+            return generarNumeroSecreto();
+        } else{
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales (){
+    asignarTextoElemento("h1", "Juego del número secreto");
+    asignarTextoElemento("p", `Escoge un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto ();
+    intentos = 1;
+}
+
+function reiniciarJuego (){
+    limpiarCaja ();
+    condicionesIniciales ();
+    document.querySelector ("#reiniciar").setAttribute ("disabled", "true");
+}
+
+condicionesIniciales ();
